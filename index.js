d4394ed88c9fa35c9cd26a7f759361460dccfc6f
GustavoSilva3164 <gustavosilva2008br@gmail.com>
2025-08-29 11:38:31 +0000

    correção dessa budega

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-const Personagen = require("./class/Personagen.js"); // corrigido para bater com o arquivo
+const Personagen = require("./class/Personagen.js");
 
-// Criando personagens fixos
 const personagem1 = new Personagen("Seimei", "Feiticeiro", 90000);
 const personagem2 = new Personagen("Shidou", "Assassino Das Sombras", 70000);
 const personagem3 = new Personagen("Hitori", "Saint Guardian", 45000);
 
-// Usando os métodos
-personagem1.atacar(500); // precisa passar o poder de ataque
+personagem1.atacar(500); 
 personagem1.mostrarInfo();
 
 personagem2.atacar(300);
