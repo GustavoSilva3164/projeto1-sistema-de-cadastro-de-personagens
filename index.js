f10e6a64a1a3c5320397d15935b2dda050f73985
GustavoSilva3164 <gustavosilva2008br@gmail.com>
2025-08-29 08:42:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const Personagen = require("./class/Personagen.js");
 
-const personagem1 = new Personagen("Seimei", "Feiticeiro", 90000);
-const personagem2 = new Personagen("Shidou", "Assassino Das Sombras", 70000);
-const personagem3 = new Personagen("Hitori", "Saint Guardian", 45000);
+const personagem1 = new Personagen("Seimei", "Feiticeiro", 8000);
+const personagem2 = new Personagen("Shidou", "Assassino Das Sombras", 7000);
+const personagem3 = new Personagen("Hitori", "Saint Guardian", 15500);
 
-personagem1.atacar(500); 
+personagem1.atacar(5000); 
 personagem1.mostrarInfo();
 
-personagem2.atacar(300);
+personagem2.atacar(3000);
 personagem2.mostrarInfo();
 
 personagem3.atacar(1200);
 personagem3.mostrarInfo();
+
